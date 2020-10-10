440283293cb1c211e73241a48bba5e5fe55f1807
Kyle Guerrero <kyleguerrero85@gmail.com>
2020-10-10 12:40:53 -0500

    config: fixing font

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Source Code Pro Nerd Font Complete:pixelsize=18:antialias=true:autohint=true";
+static char *font = "SauceCodePro Nerd Font:pixelsize=18:antialias=true:autohint=true";
 static int borderpx = 10;
 
 /*
